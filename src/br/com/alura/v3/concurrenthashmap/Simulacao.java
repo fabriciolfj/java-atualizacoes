d9a3535caeb2c3e3c0a3753c7dd025368dfc3386
fabricio <fabricio.jacob@outlook.com>
2022-02-18 20:41:14 -0300

    add

---
+++ src/br/com/alura/v3/concurrenthashmap/Simulacao.java
@@ -0,0 +1,32 @@
+package br.com.alura.v3.concurrenthashmap;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.Arrays.asList;
+
+public class Simulacao {
+
+    public static void main(String[] args) {
+        Set<Integer> threadSafeUniqueNumbers = ConcurrentHashMap.newKeySet();
+        threadSafeUniqueNumbers.add(20);
+        threadSafeUniqueNumbers.add(45);
+
+        System.out.println(threadSafeUniqueNumbers);
+
+        ConcurrentHashMap<Integer,String> numbersMap = new ConcurrentHashMap<>();
+        Set<Integer> numbersSet = numbersMap.keySet("SET-ENTRY");
+
+        numbersMap.put(1, "One");
+        numbersMap.put(2, "Two");
+        numbersMap.put(3, "Three");
+
+        System.out.println("Map before add: "+ numbersMap);
+        System.out.println("Set before add: "+ numbersSet);
+
+        numbersSet.addAll(asList(4,5));
+
+        System.out.println("Map after add: "+ numbersMap);
+        System.out.println("Set after add: "+ numbersSet);
+    }
+}
