b4f3d136885b4863caf340e31342fe5dc8fca132
fabricio <fabricio.jacob@outlook.com>
2022-03-22 13:52:19 -0300

    add

--- src/br/com/alura/v3/otimizacao/Simulacao.java
+++ src/br/com/alura/v3/otimizacao/Simulacao.java
@@ -15,6 +15,9 @@ public class Simulacao {
                 new Event("jardinopolis", 210, "t6", 7),
                 new Event("uberlandia", 187, "t7", 9));
 
+        var t = Math.abs(1 - 3);
+        System.out.println(t);
+
         Event.getNearEvents(7, events, "New York", (l1, l2) -> Math.abs(l1.hashCode() - l2.hashCode()), "t1", 10)
                 .forEach(System.out::println);
     }
